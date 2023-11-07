cfea87b40f63a9f9c90dc015b29fc7526ea705ba
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2023-11-07 12:48:29 +0100

    Add kernel config fixup

--- qbee-agent/qbee-agent.mk
+++ qbee-agent/qbee-agent.mk
@@ -23,19 +23,23 @@ define QBEE_AGENT_BUILD_CMDS
 endef
 
 define QBEE_AGENT_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/qbee-agent-$(GO_GOARCH)  $(TARGET_DIR)/usr/bin/qbee-agent
-    $(INSTALL) -d -m 0700 $(TARGET_DIR)/etc/qbee/ppkeys
-    $(INSTALL) -m 0600 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/share/ssl/ca.cert  $(TARGET_DIR)/etc/qbee/ppkeys/ca.cert
+  $(INSTALL) -m 0755 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/qbee-agent-$(GO_GOARCH) $(TARGET_DIR)/usr/bin/qbee-agent
+  $(INSTALL) -d -m 0700 $(TARGET_DIR)/etc/qbee/ppkeys
+  $(INSTALL) -m 0600 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/share/ssl/ca.cert $(TARGET_DIR)/etc/qbee/ppkeys/ca.cert
 endef
 
 define QBEE_AGENT_INSTALL_INIT_SYSTEMD
-    $(INSTALL) -D -m 0644 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/init-scripts/systemd/qbee-agent.service \
-        $(TARGET_DIR)/usr/lib/systemd/system/qbee-agent.service
+  $(INSTALL) -D -m 0644 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/init-scripts/systemd/qbee-agent.service \
+    $(TARGET_DIR)/usr/lib/systemd/system/qbee-agent.service
 endef
 
 define QBEE_AGENT_INSTALL_INIT_SYSV
-    $(INSTALL) -D -m 755 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/init-scripts/sysvinit/qbee-agent \
-        $(TARGET_DIR)/etc/init.d/S99qbee-agent
+  $(INSTALL) -D -m 755 $(@D)/qbee-agent-$(QBEE_AGENT_VERSION)/init-scripts/sysvinit/qbee-agent \
+    $(TARGET_DIR)/etc/init.d/S99qbee-agent
+endef
+
+define QBEE_AGENT_LINUX_CONFIG_FIXUPS
+  $(call KCONFIG_ENABLE_OPT,CONFIG_TUN)
 endef
 
 $(eval $(generic-package))
