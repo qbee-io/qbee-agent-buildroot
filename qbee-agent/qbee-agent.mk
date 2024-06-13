d62fb236e859fb1721afab9b6e03230850da7e50
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-06-13 09:59:02 +0200

    Release 2024.23

--- qbee-agent/qbee-agent.mk
+++ qbee-agent/qbee-agent.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-QBEE_AGENT_VERSION = 2024.19
+# keep QBEE_AGENT_COMMIT_ID in sync when updating
+QBEE_AGENT_VERSION = 2024.23
 QBEE_AGENT_SITE = $(call github,qbee-io,qbee-agent,$(QBEE_AGENT_VERSION))
 QBEE_AGENT_LICENSE = Apache-2.0
 QBEE_AGENT_LICENSE_FILES = LICENSE
 
-QBEE_AGENT_COMMIT_ID = 9d0ed1bc1bab18c75a73dbacc2026e86113c97aa
+QBEE_AGENT_COMMIT_ID = 3219c17730eab4c80598494732b4bed4c4c18b51
 
 QBEE_AGENT_GOMOD = go.qbee.io/agent
 
@@ -17,10 +18,12 @@ QBEE_AGENT_LDFLAGS = -s -w \
 	-X $(QBEE_AGENT_GOMOD)/app.Version=$(QBEE_AGENT_VERSION) \
 	-X $(QBEE_AGENT_GOMOD)/app.Commit=$(QBEE_AGENT_COMMIT_ID)
 
-define QBEE_AGENT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/bin/qbee-agent $(TARGET_DIR)/usr/bin/qbee-agent
-	$(INSTALL) -D -m 0600 $(@D)/package/share/ssl/ca.cert $(TARGET_DIR)/etc/qbee/ppkeys/ca.cert
+define QBEE_AGENT_INSTALL_CERT
+	$(INSTALL) -D -m 0600 $(@D)/package/share/ssl/ca.cert \
+		$(TARGET_DIR)/etc/qbee/ppkeys/ca.cert
 endef
+
+QBEE_AGENT_POST_INSTALL_TARGET_HOOKS += QBEE_AGENT_INSTALL_CERT
 
 define QBEE_AGENT_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(@D)/package/init-scripts/systemd/qbee-agent.service \
